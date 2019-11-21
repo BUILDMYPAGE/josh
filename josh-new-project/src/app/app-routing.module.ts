da1542ddff3ad225616c82fb800737d0805ef2a9
Joshua Reagle <joshuareagle@MacBook-Pro.local>
2019-11-21 16:36:53 -0500

    generated home page component and added to routing

--- josh-new-project/src/app/app-routing.module.ts
+++ josh-new-project/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
@@ -8,6 +9,11 @@ const routes: Routes = [
     path: 'about',
     component: AboutComponent,
     data: { title: 'About Page' }
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: { title: 'Home Page' }
   },
 ];
 
